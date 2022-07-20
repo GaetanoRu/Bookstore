1d4a4c4c61ab55544e854cfbe4d5c3a455fa4e4f
gaetano russo <gaetano-russo90@hotmail.it>
2022-07-20 11:27:07 +0200

    added book service

---
+++ Bookstore/Bookstore.BusinessLayer/Services/Interfaces/IBookService.cs
@@ -0,0 +1,15 @@
+﻿using Bookstore.Shared.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.BusinessLayer.Services.Interfaces
+{
+    public interface IBookService
+    {
+        public Task<ListResultPagination<BookListResult>> GetAsync(string search, int pageIndex, int itemsPerPage);
+        public Task<Book> GetBookAsync(Guid id);
+    }
+}
