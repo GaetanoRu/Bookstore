ec4514ff7ab36a5bcd6ed1a3ced4046c0ddb7f04
gaetano russo <gaetano-russo90@hotmail.it>
2022-07-20 11:26:49 +0200

    added  author service

---
+++ Bookstore/Bookstore.BusinessLayer/Services/AuthorService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Bookstore.BusinessLayer.Services.Interfaces;
+using Bookstore.DataAccessLayer;
+using Bookstore.Shared.Models.Responses;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.BusinessLayer.Services
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly DataContext context;
+        private readonly IMapper mapper;
+
+        public AuthorService(DataContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<IEnumerable<Author>> GetAuthorsAsync()
+        {
+            var dbAuthor = await context.Authors.ToListAsync();
+
+            var authorList = mapper.Map<IEnumerable<Author>>(dbAuthor);
+            return authorList;
+        }
+
+        public async Task<AuthorBook> GetAuthorAsyncById(Guid authorId)
+        {
+           var author = await context.Authors
+                .Include(b => b.Books)
+                .ProjectTo<AuthorBook>(mapper.ConfigurationProvider)
+                .FirstOrDefaultAsync(q => q.Id == authorId);
+
+            if (author == null)
+            {
+                return null;
+            }
+
+            return author;
+        }
+      
+    }
+}
---
+++ Bookstore/Bookstore.BusinessLayer/Services/Interfaces/IAuthorService.cs
@@ -0,0 +1,16 @@
+﻿using Bookstore.Shared.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.BusinessLayer.Services.Interfaces
+{
+    public interface IAuthorService
+    {
+        Task<AuthorBook> GetAuthorAsyncById(Guid authorId);
+        Task<IEnumerable<Author>> GetAuthorsAsync();
+
+    }
+}
