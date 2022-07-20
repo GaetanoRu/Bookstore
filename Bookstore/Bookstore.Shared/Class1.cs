67d961fc1893c0cc5c7ab9544f5ec4bb6cda647d
gaetano russo <gaetano-russo90@hotmail.it>
2022-07-20 11:25:13 +0200

    added models DTO

--- Bookstore/Bookstore.Shared/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Bookstore.Shared
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Bookstore/Bookstore.Shared/Models/Responses/BookListResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Shared.Models.Responses
+{
+    public class BookListResult
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public int Page { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+        public  Author Author { get; set; }
+        public int RatingCount { get; set; }
+        public double? RatingScore { get; set; }
+
+    }
+}
